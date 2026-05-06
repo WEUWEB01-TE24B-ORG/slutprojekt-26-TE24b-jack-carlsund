ddd8146fa0479457cb7a83f3ae516a13b99281a7
TE24b-jack-carlsund <jack.carlsund@elev.ga.ntig.se>
2026-05-06 16:29:17 +0200

    commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,6 +1,6 @@
-function bytBild()
+function bytOrmBild()
 {
-    let bild = document.getElementById("ormbild1");
+    let ormBild1 = document.getElementById("ormbild1");
 
     if (bild.src.includes("Anaconda.jpg"))
     {
@@ -15,3 +15,30 @@ function bytBild()
         bild.src = "./bilder/Anaconda.jpg"
     }
 }
+
+
+
+function bytKrokodilBild()
+{
+    let krokodilbild1 = document.getElementById("krokodilbild1");
+
+    if (bild.src.includes("Spetskrokodilen.jpg"))
+    {
+        bild.src = "./bilder/Pansarkrokodil.jpg";
+    }
+    else if(bild.src.includes("Pansarkrokodil.JPG"))
+    {
+        bild.src = "./bilder/Sötvattenskrokodil.jpg"
+    }
+    else
+    {
+        bild.src = "./bilder/Spetskrokodilen.jpg"
+    }
+}
+
+
+function spelaVideo()
+{
+    document.querySelector(".video-lada").style.display = "block"
+}
+
